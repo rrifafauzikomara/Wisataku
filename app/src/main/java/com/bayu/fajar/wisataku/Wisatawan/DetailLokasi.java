c3403a68482a1b90d68b3461eb89a9bad35ed383
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2018-07-24 21:40:47 +0700

    fix UI

--- app/src/main/java/com/bayu/fajar/wisataku/Wisatawan/DetailLokasi.java
+++ app/src/main/java/com/bayu/fajar/wisataku/Wisatawan/DetailLokasi.java
@@ -27,7 +27,7 @@ public class DetailLokasi extends AppCompatActivity {
         txt_time.setText(getIntent().getStringExtra("time"));
         txt_desk.setText(getIntent().getStringExtra("deskripsi"));
 
-        //membuat back button toolbar
+        //membuat back button
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Detail Wisata");
         setSupportActionBar(toolbar);
